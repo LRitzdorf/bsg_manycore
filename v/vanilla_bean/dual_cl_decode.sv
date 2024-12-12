d8a2b8ff69577cb1f8fdb732ea56a0e86bf7d015
Keith Meng Phou <phoukei@cs.washington.edu@kiska.cs.washington.edu>
2024-12-11 18:06:08 -0800

    Began on cases for single issue instructs

--- v/vanilla_bean/dual_cl_decode.sv
+++ v/vanilla_bean/dual_cl_decode.sv
@@ -37,10 +37,36 @@ import bsg_manycore_pkg::*;
     // check if the second address uses rs1
     logic write_read_dependency = cl_decode_inst1.write_rd & (cl_decode_inst1.rd == cl_decode_inst2.rs1 | cl_decode_inst1.rd == cl_decode_inst2.rs2) & cl_decode_inst2.read_rs2;
     logic write_write_dependency = cl_decode_inst1.write_rd & cl_decode_inst2.write_rd & (cl_decode_inst1.rd == cl_decode_inst2.rd);
-    
+
     logic has_dependency = write_read_dependency | write_write_dependency;    
 
     // single issue any op codes that mess with program counter
+    // single issue any op codes that mess with program counter
+    // When to *NOT* dual-issue instructions:
+    // - First instruction is a special operation:
+    //   - Fence
+    //   - Send/recv barrier
+    //   - CSR
+    //   - MRET
+    //   - Conditional branch
+    // - Two instructions of same type (INT/FP)
+
+    logic single_issue_pc_op, single_issue_same_type, single_issue_special;
+
+    //assign single_issue_pc_op
+
+    // Should we consider no op intructions for single issue as well?
+    localparam instruction_t NO_OP = 32'h00000013; // Standard no-op RISC V
+
+    assign single_issue_same_type = 
+        (decode_intermediate[0].is_fp_op && decode_intermediate[1].is_fp_op) || // both FP
+        (!decode_intermediate[0].is_fp_op && !decode_intermediate[1].is_fp_op); // both INT/regular instruction, this check might have false positives?
+
+    // single issue any special instructons
+    assign  single_issue_special = 
+
+    //assign do_single_issue = has_dependency || any single issue cond 
+
 
     // OR both lower level decoders
     assign decode_o = decode_intermediate[0] | decode_intermediate[1]; 
