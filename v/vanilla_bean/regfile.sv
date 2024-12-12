70a96871b6ea86c7dcb069dc6b9dd6e7ea5fd3e9
Lucas Ritzdorf <42657792+LRitzdorf@users.noreply.github.com>
2024-12-12 11:51:41 -0800

    regfile: support multiple write ports (parametrized)

--- v/vanilla_bean/regfile.sv
+++ v/vanilla_bean/regfile.sv
@@ -13,6 +13,7 @@
 module regfile
   #(`BSG_INV_PARAM(width_p)
     , `BSG_INV_PARAM(els_p)
+    , `BSG_INV_PARAM(num_rd_p)
     , `BSG_INV_PARAM(num_rs_p)
     , `BSG_INV_PARAM(x0_tied_to_zero_p)
     , harden_p=0
@@ -23,9 +24,9 @@ module regfile
     input clk_i
     , input reset_i
 
-    , input w_v_i
-    , input [addr_width_lp-1:0] w_addr_i
-    , input [width_p-1:0] w_data_i
+    , input [num_rd_p-1:0] w_v_i
+    , input [num_rd_p-1:0][addr_width_lp-1:0] w_addr_i
+    , input [num_rd_p-1:0][width_p-1:0] w_data_i
     
     , input [num_rs_p-1:0] r_v_i
     , input [num_rs_p-1:0][addr_width_lp-1:0] r_addr_i
@@ -37,6 +38,8 @@ module regfile
     regfile_hard #(
       .width_p(width_p)
       ,.els_p(els_p)
+      // FIXME: This would eventually need support for multiple write ports!
+      // ,.num_rd_p(num_rd_p)
       ,.num_rs_p(num_rs_p)
       ,.x0_tied_to_zero_p(x0_tied_to_zero_p)
     ) rf (.*);
@@ -45,6 +48,7 @@ module regfile
     regfile_synth #(
       .width_p(width_p)
       ,.els_p(els_p)
+      ,.num_rd_p(num_rd_p)
       ,.num_rs_p(num_rs_p)
       ,.x0_tied_to_zero_p(x0_tied_to_zero_p)
     ) rf (.*);
--- v/vanilla_bean/regfile_synth.sv
+++ v/vanilla_bean/regfile_synth.sv
@@ -11,6 +11,7 @@
 module regfile_synth
   #(`BSG_INV_PARAM(width_p)
     , `BSG_INV_PARAM(els_p)
+    , `BSG_INV_PARAM(num_rd_p)
     , `BSG_INV_PARAM(num_rs_p)
     , `BSG_INV_PARAM(x0_tied_to_zero_p)
 
@@ -20,9 +21,9 @@ module regfile_synth
     input clk_i
     , input reset_i
 
-    , input w_v_i
-    , input [addr_width_lp-1:0] w_addr_i
-    , input [width_p-1:0] w_data_i
+    , input [num_rd_p-1:0] w_v_i
+    , input [num_rd_p-1:0][addr_width_lp-1:0] w_addr_i
+    , input [num_rd_p-1:0][width_p-1:0] w_data_i
     
     , input [num_rs_p-1:0] r_v_i
     , input [num_rs_p-1:0][addr_width_lp-1:0] r_addr_i
@@ -48,8 +49,10 @@ module regfile_synth
       assign r_data_o[i] = (r_addr_r[i] == '0)? '0 : mem_r[r_addr_r[i]];
 
     always_ff @ (posedge clk_i)
-      if (w_v_i & (w_addr_i != '0))
-        mem_r[w_addr_i] <= w_data_i;
+      for (genvar i = 0; i < num_rd_p; i++)
+        if (w_v_i[i] & (w_addr_i[i] != '0))
+          // NOTE: In case of write address overlap, higher-indexed port wins
+          mem_r[w_addr_i[i]] <= w_data_i[i];
 
 
   end
@@ -61,8 +64,10 @@ module regfile_synth
       assign r_data_o[i] = mem_r[r_addr_r[i]];
 
     always_ff @ (posedge clk_i)
-      if (w_v_i)
-        mem_r[w_addr_i] <= w_data_i;
+      for (genvar i = 0; i < num_rd_p; i++)
+        if (w_v_i[i])
+          // NOTE: In case of write address overlap, higher-indexed port wins
+          mem_r[w_addr_i[i]] <= w_data_i[i];
     
   end
 
